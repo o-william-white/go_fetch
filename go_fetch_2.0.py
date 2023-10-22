85ae560852d977bdbfa50eabb104e2628b1ae888
Ollie White <o.william.white@gmail.com>
2023-10-22 21:50:33 +0100

    Update to go_fetch

--- go_fetch_2.0.py
+++ go_fetch_2.0.py
@@ -11,14 +11,14 @@ argparse_description = '''
 '''
 
 argparse_usage = '''
-
+python3 go_fetch_2.0.py --taxonomy "Arabidopsis lyrata" --target chloroplast --db genbank --email o.william.white@gmail.com
 '''
 
 # argparse
 parser = argparse.ArgumentParser(description=argparse_description, usage=argparse_usage)
 parser.add_argument('--taxonomy',     help='Taxonomy of rank to search for e.g. "Arabidopsis"', type=str, required=True)
-#parser.add_argument('--target',       help='Target sequence type.', choices=['chloroplast', 'mitochondrion', 'ribosomal'], required=True)
-#parser.add_argument('--db',           help='Databse to search/download from. Either refseq (NCBI) or genbank (INSDC). Default=refseq.', choices=['refseq', 'genbank'], required=False, default='refseq')
+parser.add_argument('--target',       help='Target sequence type.', choices=['chloroplast', 'mitochondrion', 'ribosomal'], required=True)
+parser.add_argument('--db',           help='Databse to search/download from. Either refseq (NCBI) or genbank (INSDC). Default=refseq.', choices=['refseq', 'genbank'], required=False, default='refseq')
 #parser.add_argument('--name',         help='Database name. Required with --download.', required=False)
 #parser.add_argument('--min',          help='Minimum number of target sequences to download. Required with --download.', type=int, required=False)
 #parser.add_argument('--max',          help='Maximum number of target sequences to download. Must be larger than --min. Required with --download.',       type=int, required=False)
@@ -40,7 +40,14 @@ Entrez.email = args.email
 
 ### functions
 
-# check if taxonomy exists
+# get taxonomic id from scientific name
+def get_taxonomic_id(taxonomy):
+    handle = Entrez.esearch(db="Taxonomy", term=f"{taxonomy}[Scientific Name]")
+    record = Entrez.read(handle)
+    return str(record["IdList"][0])
+assert get_taxonomic_id("Arabidopsis thaliana") == "3702"
+
+# check if taxonomy id exists
 def taxid_exists(taxid):
     handle = Entrez.efetch(db="taxonomy", id=taxid)
     record = Entrez.read(handle)
@@ -52,12 +59,22 @@ assert taxid_exists(3701) == True
 assert taxid_exists(123456789) == False
 
 # get scientific name from taxonomic id
-def get_scientific_name(taxid):
-    handle = Entrez.efetch(db="Taxonomy", id=taxid)
+def get_scientific_name(taxonomy):
+    handle = Entrez.efetch(db="Taxonomy", id=taxonomy)
     record = Entrez.read(handle)
     return record[0]["ScientificName"]
-
 assert get_scientific_name("3702") == "Arabidopsis thaliana"
+
+# check if taxonomy name exists
+def scientific_name_exists(taxonomy):
+    handle = Entrez.esearch(db="Taxonomy", term=f"{taxonomy}[Scientific Name]")
+    record = Entrez.read(handle)
+    if int(record["Count"]) >= 1:
+        return True
+    else:
+        return False
+assert scientific_name_exists("Arabidopsis") == True
+assert scientific_name_exists("NotArabidopsis") == False
 
 # get lineage from taxid
 def get_lineage(taxid):
@@ -70,6 +87,16 @@ def get_lineage(taxid):
     return lineage
 
 assert get_lineage(3701) == ['Camelineae', 'Brassicaceae', 'Brassicales', 'malvids', 'rosids', 'Pentapetalae', 'Gunneridae', 'eudicotyledons', 'Mesangiospermae', 'Magnoliopsida', 'Spermatophyta', 'Euphyllophyta', 'Tracheophyta', 'Embryophyta', 'Streptophytina', 'Streptophyta', 'Viridiplantae', 'Eukaryota', 'cellular organisms']
+
+def get_children(taxonomy):
+    handle = Entrez.esearch(db='taxonomy', term=f"{taxonomy}[next level]", retmax=9999)
+    record = Entrez.read(handle)
+    children = []
+    for i in record["IdList"]:
+        children.append(get_scientific_name(i))
+    return(children)
+assert get_children("Arabidopsis lyrata") == ['Arabidopsis petraea subsp. umbrosa', 'Arabidopsis petraea subsp. septentrionalis', 'Arabidopsis lyrata subsp. lyrata', 'Arabidopsis lyrata subsp. petraea']
+
 
 # generate search term
 # target = 'chloroplast', 'mitochondrion', 'robosomal'
@@ -89,6 +116,56 @@ def search_term(taxid, target, db):
         term += f" AND ddbj_embl_genbank[filter]"
     return term 
 
-print(search_term(get_scientific_name(3701), "chloroplast", "refseq"))
+### main
 
+# get and check ncbi taxonomy_id and scientific_name
+try:
+    int(args.taxonomy)
+    print("Taxonomy input is a numeric NCBI ID.")
+    # check if taxonomy id exists
+    if not taxid_exists(args.taxonomy):
+        sys.exit(f"Taxonomy ID {args.taxonomy} does not exist. Please check for the correct taxonomy id on https://www.ncbi.nlm.nih.gov/taxonomy")
+    else:
+        print("Taxonomy ID exists on NCBI")
+    taxonomy_id = args.taxonomy
+    taxonomy_name = get_scientific_name(taxonomy_id)
+except:
+    print("Taxonomy input is a scientific name.")
+    if not scientific_name_exists(args.taxonomy):
+        sys.exit(f"Scientific name {args.taxonomy} does not exist. Please check for the correct taxonomy id on https://www.ncbi.nlm.nih.gov/taxonomy")
+    taxonomy_name = args.taxonomy
+    taxonomy_id = get_taxonomic_id(taxonomy_name)
+
+print("NCBI Id: " + taxonomy_id)
+print("Scientific name: " + taxonomy_name + "\n")
+
+# get lineage
+lineage = get_lineage(taxonomy_id)
+lineage.insert(0, taxonomy_name)
+
+# get children
+children = get_children(taxonomy_name)
+
+spacer = ""
+for l in lineage[::-1]:
+    print(f"{spacer}{l}")
+    if spacer == "":
+        spacer = "|_" + spacer
+    else: 
+        spacer = "  " + spacer
+for c in children: 
+    print(f"{spacer}{c}")
+
+# define search term
+term = search_term(args.taxonomy, args.target, args.db)
+print(f"\nUsing search term = {term}")
+
+# count the number of sequences on ncbi using the term generated
+def target_count(input_term):
+    # esearch
+    handle = Entrez.esearch(db='Nucleotide', term=input_term, retmax=999)
+    record = Entrez.read(handle)
+    return record["Count"]
+
+print(target_count(term))
 
